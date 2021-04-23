a4ad84e0e8396d2d349fde94303e1dad5d014de9
twdavidong <64457327+twdavidong@users.noreply.github.com>
2021-04-23 22:04:11 +0800

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator} from "@react-navigation/stack";
+import { NavigationContainer} from "@react-navigation/native";
+import BlockRGB from "./components/BlockRGB";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+
+function HomeScreen() {
+   const [colorArray, setColorArray] = useState([
+        { red:255, green:0, blue:0, id: "0"},
+        { red:0, green:255, blue:0, id: "1"},
+        { red:0, green:0, blue:255, id:"2"}
+   ]);
+
+   function renderItem({item}) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+    }
+
+    return (
+
+      <View style={styles.container}>
+        <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
+      </View>
   );
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
